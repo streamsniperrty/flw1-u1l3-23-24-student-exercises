3b2c510162ef57520f95be402e88b227af328acc
streamsniperrty <54614860+streamsniperrty@users.noreply.github.com>
2023-10-26 22:23:10 +0000

    FIrst commit - 10/26

--- magicalCreature.js
+++ magicalCreature.js
@@ -1,36 +1,36 @@
 // Welcome to the magical zoo! Your task is to create and manage a list of magical creatures. Let's have some fun!
 
 // 1. Declare a variable named 'creatureName' and assign it a magical creature's name as a string.
-
+let creatureName = "Ghost";
 
 
 // 2. Declare a variable named 'creatureDescription' and describe its magical properties as a string.
-
+let creatureDescription = "can fly, shoot blasts of lightning, and radiate ghostly waves";
 
 // 3. Do you believe that this creature is friendly to humans? 
 // Declare a variable named 'isFriendly' and assign it a boolean value.
-
+let isFriendly = true;
 
 
 // 4. The magical zoo has an age restriction! Declare a variable named 'visitorAge' and assign it a random age between 1 and 60.
 // Hint: Use Math.floor() to round a number up.
 // Hint: Use Math.random() for generating random numbers.
-
-
+let visitorAge = Math.floor(Math.random() * 60);
+console.log(visitorAge)
 
 // 5. Determine if the visitor is allowed to see the creature.
 // Declare a variable named 'canVisit' and assign it an expression to see 
 // if the visitorAge variable is greater than 15.
 // Hint: use this resource if you need support: https://www.w3schools.com/js/js_booleans.asp 
-
-
+let canVisit = visitorAge > 15;
+console.log(canVisit);
 
 // 6. Combine all the information into a summary.
 // Declare a variable named 'zooSummary' and combine all the above information using concatenation.
-
+let zooSummary = "The " + creatureName + " is a creature that " + creatureDescription + ". It is " + isFriendly + " that it is friendly to humans. The creature is " + visitorAge + " years old. It is " + canVisit + " that it can visit.";
 
 // 7. Print the summary using console.log()
-
+console.log(zooSummary);
 
 
 // Challenge:
